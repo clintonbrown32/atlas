3e0c6591708cff7b4655bfa419a0debdc27275d7
clintonbrown32 <109828661+clintonbrown32@users.noreply.github.com>
2023-07-04 19:55:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+document.getElementById('searchInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        // Perform search here
+        console.log('Search executed:', this.value);
+    }
+});
